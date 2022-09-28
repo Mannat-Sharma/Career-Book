b0c3ddff28b774f31e5b6a9df86b81502eebc874
Mannat <sharmamannat.25.11@gmail.com>
2022-09-28 10:32:06 +0530

    Next Update

--- src/components/Home.js
+++ src/components/Home.js
@@ -6,10 +6,11 @@ export default function Home(){
                 <h1>Online education in an easy way!</h1>
                 <h3>CareerBook offers various free courses and resources for your skill development.</h3>
                 <h3>Lets choose the right dream!</h3>
+                <a href="/explore">Explore Careers</a>
+
             </div>
             <div>     
             <img src="https://cdni.iconscout.com/illustration/premium/thumb/girl-student-learning-online-2749772-2294224.png"></img>
-            <button>Explore Careers</button>
             </div>
         </div>
     )
